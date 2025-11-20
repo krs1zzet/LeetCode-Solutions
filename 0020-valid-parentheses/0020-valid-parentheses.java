83784b10dcb2284e342243f84d427f8235ca5173
İzzettin Karasayar <115171248+krs1zzet@users.noreply.github.com>
2025-11-20 11:15:56 +0300

    Time: 3 ms (67.35%), Space: 43.1 MB (22.38%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,19 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public boolean isValid(String s) {
+        if ((s.length() & 1) == 1) return false;
+
+        Deque<Character> st = new ArrayDeque<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') st.push(')');
+            else if (ch == '[') st.push(']');
+            else if (ch == '{') st.push('}');
+            else {
+                if (st.isEmpty() || st.pop() != ch) return false;
+            }
+        }
+        return st.isEmpty();
+    }
+}
