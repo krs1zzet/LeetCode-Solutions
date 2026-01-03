a82e31e186be6780a3247b9fd61edbf8cd8ea0b4
İzzettin Karasayar <115171248+krs1zzet@users.noreply.github.com>
2026-01-03 14:28:51 +0300

    Time: 0 ms (100%), Space: 42.8 MB (71.63%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -1,18 +1,18 @@
 class Solution {
     public int minimumMoves(String s) {
-        int moves = 0;
-        int i = 0;
+        int move = 0;
         int n = s.length();
+        int i =0;
 
-        while (i < n) {
-            if (s.charAt(i) == 'X') {
-                moves++;
-                i += 3; 
-            } else {
+        while(i<n){
+            if(s.charAt(i) == 'X'){
+                move++;
+                i+=3;
+            }
+            else{
                 i++;
             }
         }
-
-        return moves;
+        return move;
     }
 }
